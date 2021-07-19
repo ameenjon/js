679e041a28b6fbc4ec580bda60001045f434949c
Aminjon Ergashev <ameenjon@mail.ru>
2021-07-20 00:06:06 +0500

    Domashka

---
+++ ht2.js
@@ -0,0 +1,6 @@
+let cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+let volvo = cars[1]
+cars[0] = 'Ford'
+console.log(cars);
+console.log(cars.length);
---
+++ scr.js
@@ -0,0 +1,10 @@
+let arr = ['Джаз', 'Блюз']
+arr.push('Рок-н-ролл')
+arr[1] = 'Классика'
+
+
+console.log(arr.shift());
+arr.unshift('Рэгги') 
+arr.unshift('Рэп') 
+
+console.log(arr)
